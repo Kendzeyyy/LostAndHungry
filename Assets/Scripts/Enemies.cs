00513ee40fc88b459c77789cb99e59f26d2a428e
Jenna Kopra <Jenna Kopra>
2016-11-21 14:02:26 +0200

    erik death animation

--- Assets/Scripts/Enemies.cs
+++ Assets/Scripts/Enemies.cs
@@ -5,8 +5,10 @@ public class Enemies : MonoBehaviour {
 	private GameObject enemy;
 
 	void OnCollisionEnter2D (Collision2D coll){
+		
 		Debug.Log("Hit");
 		Destroy(gameObject);
+
 	}
 
 	// Use this for initialization
--- Assets/Scripts/ErikController.cs
+++ Assets/Scripts/ErikController.cs
@@ -1,43 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ErikController : MonoBehaviour {
+public class ErikController : MonoBehaviour
+{
 	private ButtonController upButton;
 	private ButtonController downButton;
-
+	private Animator animator;
 	public float erikSpeed;
 
 	private GameObject erik;
-	
-	// Use this for initialization
-	void Start () {
+
+	void awake() {
+		animator = GetComponent<Animator>();
+		// Use this for initialization
+	}
+	void OnCollisionEnter2D (Collision2D coll)
+	{
+		animator.SetInteger ("die", 1);
+		Debug.Log ("Hit");
+
+	}
+
+	void Start ()
+	{
+		
 		downButton = GameObject.Find ("DownButton").GetComponent<ButtonController> ();
 		upButton = GameObject.Find ("UpButton").GetComponent<ButtonController> ();
-
 		erik = GameObject.Find ("ErikPlayer");
 
 	}
 
 	// Update is called once per frame
-	void Update () {
-		erik.transform.Translate (0.05f,0,0);
-		if (upButton.GetPressed ()){
+	void Update ()
+	{
+		erik.transform.Translate (0.05f, 0, 0);
+		if (upButton.GetPressed ()) {
 			MoveErik ("up");
 		}
 		if (downButton.GetPressed ()) {
 			MoveErik ("down");
 		}
 	}
-	void MoveErik(string direction){
+
+	void MoveErik (string direction)
+	{
 		Debug.Log ("move " + direction);
 
 
-		if (direction.Equals("up")) {
-			erik.transform.Translate (0,0.4f,0);
+		if (direction.Equals ("up")) {
+			erik.transform.Translate (0, 0.4f, 0);
 		}
 
-		if (direction.Equals("down")) {
-			erik.transform.Translate (0,0,0);
+		if (direction.Equals ("down")) {
+			erik.transform.Translate (0, 0, 0);
 		}
 	}
 
