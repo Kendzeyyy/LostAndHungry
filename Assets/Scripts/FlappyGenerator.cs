ad8e3a525552b5ec0c0271e1e167d1f1b2cbd895
Mikael Ahlström <mikael.ahlstrom@metropolia.fi>
2016-11-22 09:35:04 +0200

    Lisää scriptit esteiden generoinnille ja kosketusohjaukselle.

---
+++ Assets/Scripts/FlappyGenerator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObstacle : MonoBehaviour {
+
+	public GameObject obstacle;
+	float x = 0;
+
+	void Update () {
+		float y = Random.Range(15.43331f, 29.42488f);
+		if(x < 1000) {
+			Instantiate(obstacle, new Vector3(x * 6.0f, y, 0),Quaternion.identity);
+			x++;
+		}
+		Debug.Log(x);
+	}
+}
---
+++ Assets/Scripts/SwipeControls.cs
@@ -0,0 +1,40 @@
+﻿
+//inside class
+Vector2 firstPressPos;
+Vector2 secondPressPos;
+Vector2 currentSwipe;
+
+public void Swipe()
+{
+	if(Input.touches.Length > 0)
+	{
+		Touch t = Input.GetTouch(0);
+		if(t.phase == TouchPhase.Began)
+		{
+			//save began touch 2d point
+			firstPressPos = new Vector2(t.position.x,t.position.y);
+		}
+		if(t.phase == TouchPhase.Ended)
+		{
+			//save ended touch 2d point
+			secondPressPos = new Vector2(t.position.x,t.position.y);
+
+			//create vector from the two points
+			currentSwipe = new Vector3(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+			//normalize the 2d vector
+			currentSwipe.Normalize();
+
+			//swipe upwards
+			if(currentSwipe.y > 0  currentSwipe.x > -0.5f  currentSwipe.x < 0.5f)
+			{
+				Debug.Log("up swipe");
+			}
+			//swipe down
+			if(currentSwipe.y < 0  currentSwipe.x > -0.5f  currentSwipe.x < 0.5f)
+			{
+				Debug.Log("down swipe");
+			}
+		}
+	}
+}
