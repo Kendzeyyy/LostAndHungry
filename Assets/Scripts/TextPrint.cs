7b08ed8f3d7d31cf5aa452057039629f1dce00ce
Mikael Ahlström <mikael.ahlstrom@metropolia.fi>
2016-11-22 13:47:35 +0200

    luo toimiva script hitaalle tekstintulostukselle.

--- Assets/Scripts/TextPrint.cs
+++ Assets/Scripts/TextPrint.cs
@@ -1,35 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
 
-public class TextDisplayer : MonoBehaviour {
+// attach to UI Text component (with the full text already there)
 
-	public string textShownOnScreen;
-	public string fullText = "The text you want shown on screen with typewriter effect.";
-	public float wordsPerSecond = 2; // speed of typewriter
-	private float timeElapsed = 0;
-	 //private int timeElapsedInt = System.Convert.ToInt32(timeElapsed);
+public class TextPrint : MonoBehaviour 
+{
 
-	// Update is called once per frame
-	void Update () {
-		timeElapsed += Time.deltaTime;
-		textShownOnScreen = GetWords(fullText, timeElapsed * wordsPerSecond);
-	}
-	private string GetWords(string text, float wordCount)
+	Text txt;
+	string story;
+
+	void Awake () 
 	{
-		float words = wordCount;
-		// loop through each character in text
-		for (int i = 0; i < text.Length; i++)
-		{ 
-			if (text[i] == ' ')
-			{
-				words--;
-			}
-			if (words <= 0)
-			{
-				return text.Substring(0, i);
-			}
-		}
-		return text;
+		txt = GetComponent<Text> ();
+		story = txt.text;
+		txt.text = "";
+
+		// TODO: add optional delay when to start
+		StartCoroutine ("PlayText");
 	}
+
+	IEnumerator PlayText()
+	{
+		foreach (char c in story) 
+		{
+			txt.text += c;
+			yield return new WaitForSeconds (0.025f);
+		}
+	}
+
 }
