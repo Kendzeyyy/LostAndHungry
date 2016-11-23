6825f4355eff321013c340a99093a421b953e7cb
Mikael Ahlström <mikael.ahlstrom@metropolia.fi>
2016-11-23 23:30:25 +0200

    Luo testi Scene ObstacleGeneration joka toimii täydellisesti

--- Assets/Scripts/FlappyGenerator.cs
+++ Assets/Scripts/FlappyGenerator.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SpawnObstacle : MonoBehaviour {
+public class FlappyGenerator : MonoBehaviour {
 
 	public GameObject obstacle;
-	float x = 0;
+
+	// obstacle generation start point
+	float x = 10;
 
 	void Update () {
-		float y = Random.Range(15.43331f, 29.42488f);
+
+		// obstacles min & max height
+		float y = Random.Range(-69.3f, -56.7f);
+
+		// amount of generated obstacles and point counter
 		if(x < 1000) {
 			Instantiate(obstacle, new Vector3(x * 6.0f, y, 0),Quaternion.identity);
 			x++;
