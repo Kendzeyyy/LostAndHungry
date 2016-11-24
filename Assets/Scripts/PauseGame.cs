2ba9cd4bdbdd72252eecf79873bbefc93186a615
Kendy Nguyen <kendynguyen20@hotmail.com>
2016-11-24 22:18:28 +0200

    kommentit lisätty

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -4,47 +4,47 @@ using UnityEngine.UI;
 
 public class PauseGame : MonoBehaviour {
 
-	//----------Variables-----------------------------------------------------------------------------------------------------
+	//----------Variables-----------------------------------------------------------------------------------------------------------------------------------------
 
-	public Button pause;
-	private GameObject tausta;
-	bool esimerkki = false;
+	public Button pause;													// pause nappi
+	private GameObject tausta;												// peli objekti tausta
+	bool esimerkki = false;													// esimerkki boolean arvo = false
 
-	//-----------Start-------------------------------------------------------------------------------------------------------
+	//-----------Start--------------------------------------------------------------------------------------------------------------------------------------------
 
 	void Start (){
-		tausta = GameObject.Find ("Canvas");
-		tausta.SetActive (false);
-		pause = GameObject.Find ("PauseLogo").GetComponent<Button> ();
-		Debug.Log (pause);
-		pause.onClick.AddListener(()=> Pause());
+		tausta = GameObject.Find ("Canvas");								// etsii Unity ohjelmasta "Canvasta"
+		tausta.SetActive (false);											// kun peli käynistyy, asettaa canvaksen pois päältä
+		pause = GameObject.Find ("PauseLogo").GetComponent<Button> ();		// etsii Unity ohjelmasta "PauseLogo"
+		Debug.Log (pause);													// 
+		pause.onClick.AddListener(()=> Pause());							// 
 	}
 
-	//-----------Update-------------------------------------------------------------------------------------------------------
+	//-----------Update-------------------------------------------------------------------------------------------------------------------------------------------
 
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Escape))
+		if (Input.GetKeyDown(KeyCode.Escape))								// jos painaa kerran "Esc"
 		{
-			Pause();
+			Pause();														// ohjelma pysähtyy
 		}
 	}
 
-	//------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 	public void Pause()
 	{
-		if (esimerkki)
+		if (esimerkki)														// jos (esimerkki)
 		{
-			esimerkki = !esimerkki;
-			tausta.SetActive(true);
-			Time.timeScale = 0;
+			esimerkki = !esimerkki;											// esimerkki = epätosi esimerkki
+			tausta.SetActive(true);											// tausta.SetACtive (Canvas) on tosi eli pause menu on päällä
+			Time.timeScale = 0;												// asettaa pelin ympäristölle ajaksi 0 eli kaikki pysähtyy paitsi musiikki
 		}
 
-		else
+		else  																// muuten
 		{
-			esimerkki = !esimerkki;
-			tausta.SetActive(false);
-			Time.timeScale = 1;
-		}
+			esimerkki = !esimerkki;											// esimerkki = epätosi esimerkki
+			tausta.SetActive(false);										// tausta.SetACtive (Canvas) on epätosi eli pause menu menee pois päältä
+			Time.timeScale = 1;												// asettaa pelin ympäristölle ajaksi 1 eli normaali aika
+		}																	// Jos arvoksi asettaa esim. 2 niin peli on 2x nopeampi kuin normaalisti
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,28 +5,28 @@ public class PlayerController : MonoBehaviour {
 
 //-----------Variables--------------------------------------------------------------------------------------------------------------
 
-	private ButtonController upButton;
-	private ButtonController downButton;
-	public float jump;
-	float moveVelocity;
-	bool grounded = false;
+	private ButtonController upButton;															// nappiYlös
+	private ButtonController downButton;														// nappiAlas
+	public float jump;																			// hyppy
+	float moveVelocity;																			// liikkumisnopeus
+	bool grounded = false;																		// bool grounded arvo false
 
-//----------------------------------------------------------------------------------------------------------------------------------
+//-----------Start------------------------------------------------------------------------------------------------------------------
 
-	void Start (){
-		upButton = GameObject.Find ("ButtonUp").GetComponent<ButtonController> ();
-		downButton = GameObject.Find ("ButtonDown").GetComponent<ButtonController> ();
+	void Start (){																				// alkaa heti kun käynnistää pelin
+		upButton = GameObject.Find ("ButtonUp").GetComponent<ButtonController> ();				// etsii "ButtonUp" Unity ohjelmasta
+		downButton = GameObject.Find ("ButtonDown").GetComponent<ButtonController> ();			// etsii "ButtonDown" Unity ohjelmasta
 	}
 
-//----------------------------------------------------------------------------------------------------------------------------------
+//-----------Update-----------------------------------------------------------------------------------------------------------------
 
 	void Update () {
 
 		// Hyppääminen
-		if (upButton.GetPressed()){
-			if (grounded){
-				GetComponent<Rigidbody2D> ().velocity = new Vector2 (
-					GetComponent<Rigidbody2D> ().velocity.x, jump);
+		if (upButton.GetPressed()){																// jos upButton eli "ButtonUp" painetaan
+			if (grounded){																		// jos grounded = false
+				GetComponent<Rigidbody2D> ().velocity = new Vector2 (							// hakee component liikkumisvauhti = Vector2
+					GetComponent<Rigidbody2D> ().velocity.x, jump);								// liikkumisvauhti x suunnassa, hyppy
 
 			}
 		}
@@ -35,10 +35,10 @@ public class PlayerController : MonoBehaviour {
 //------------------------------------------------------------------------------------------------------------------------------------ 
 
 	// Tarkistaa onko grounded vai ei
-	void OnTriggerEnter2D(){
-		grounded = true;
+	void OnTriggerEnter2D(){																	// tarkistaa onko hahmo maassa
+		grounded = true;																		// jos on maassa niin grounded arvo on true
 	}
-	void OnTriggerExit2D(){
-		grounded = false;
+	void OnTriggerExit2D(){																		// tarkistaa onko hahmo maassa
+		grounded = false;																		// jos ei ole maassa niin grounded arvo on false
 	}
 }
