a5e6b2cf65fe9e632f087a5d845941384257617b
Mikael Ahlström <mikael.ahlstrom@metropolia.fi>
2016-11-28 13:38:48 +0200

    enemy generation

--- Assets/Scripts/FloorGenerator1.cs
+++ Assets/Scripts/FloorGenerator1.cs
@@ -3,26 +3,26 @@ using System.Collections;
 
 /*
 Generates any given objects into the game randomly, between the given values.
-This script is meant to create floor in to the JetPack level.
+This script is meant to create floor in to levels.
 Attach floor object into this script in Unity.
 author: Ahlström
 */
 
-public class FloorGenerator : MonoBehaviour {
+public class FloorGenerator1 : MonoBehaviour {
 
 	public GameObject floor;
 
 	// floor generation start point
-	float x = 0;
+	float x = 8;
 
 	void Update () {
 
 		// obstacles min & max height
-		float y = Random.Range(-11.03f, -11.03f);
+		float y = Random.Range(150f, 150f);
 
 		// amount of floor created amd distance between them
-		if(x < 200) {
-			Instantiate(floor, new Vector3(x * 10.0f, y, 0),Quaternion.identity);
+		if(x < 2200) {
+			Instantiate(floor, new Vector3(x * 250.0f, y, 0),Quaternion.identity);
 			x++;
 		}
 		Debug.Log(x);
---
+++ Assets/Scripts/LevelGenerator1.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+Generates any given objects into the game randomly, between the given values.
+Attach any GameObject into this script in Unity.
+author: Ahlström
+*/
+
+public class LevelGenerator1 : MonoBehaviour {
+
+	public GameObject enemy1;
+	public GameObject enemy2;
+
+	// enemy generation start point.
+	float x = 2;
+
+	void Start () {
+
+		// amount of enemies created
+		int counter = 0;
+		while (counter<257){
+			
+		float lottery = Random.value;
+
+		if(lottery <= 0.5) { 
+		// enemys min & max height. All enemies created inside these values.
+				float y = Random.Range(400f, 400f);
+
+		// Sets the max amount of generated obstacles and space between them.
+			Instantiate(enemy1, new Vector3(x * 2000.0f, y, 0),Quaternion.identity);
+				counter++;
+				x++;
+		} else {
+			// Obstacles min & max height. All objects created inside these values.
+				float y = Random.Range(500f, 500f);
+
+			// Sets the max amount of generated obstacles and space between them.
+			Instantiate(enemy2, new Vector3(x * 2000.0f, y, 0),Quaternion.identity);
+				counter++;
+				x++;
+
+		}
+		}
+		}
+	}
