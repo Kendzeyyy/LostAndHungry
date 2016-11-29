9f5defd1ec15454dc1fe2c832aa95875bb5b16ff
Kendy Nguyen <kendynguyen20@hotmail.com>
2016-11-29 13:42:30 +0200

    PauseMenu&MainMenu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class StartMenu : MonoBehaviour {
+
+
+//------PlayButton--------------------------------------------------------------------------------------------------
+
+	public void Play (){
+		SceneManager.LoadScene ("Jet");		// lataa scenen
+
+	}
+
+//--------TextButton------------------------------------------------------------------------------------------------
+
+	public void Story (){
+		SceneManager.LoadScene ("StoryText");
+	}
+
+	public void StoryTwo (){
+		SceneManager.LoadScene ("StoryText2");
+	}
+
+	public void StoryThree (){
+		SceneManager.LoadScene ("StoryText3");
+	}
+
+	public void StoryFour (){
+		SceneManager.LoadScene ("StoryText4");
+	}
+
+//------------------------------------------------------------------------------------------------------------------
+
+	void Start (){
+		Time.timeScale = 1;
+
+	}
+
+}
