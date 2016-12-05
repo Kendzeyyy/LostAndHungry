dccf1708862f0584975389fdd3a13684f59f6432
Mikael Ahlström <mikael.ahlstrom@metropolia.fi>
2016-12-05 12:39:23 +0200

    RespectCounter

--- Assets/Scripts/RespectCounter.cs
+++ Assets/Scripts/RespectCounter.cs
@@ -1,31 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-
+ 
 public class RespectCounter : MonoBehaviour {
-
-	private float score = 0.0f;
-
-	public Text scoreText;
-	private bool isDead = false;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-		if(isDead) {
-			return;
-		}
-
-		score += Time.deltaTime;
-		scoreText.text = ((int)score).ToString();
-	}
-
-	public void OnDeath() {
-		isDead = true;
-	}
+ 
+    private float score = 0.0f;
+ 
+    public Text scoreText;
+    private bool isDead = false;
+    public DeathMenu deathMenu;
+ 
+    // Use this for initialization
+    void Start () {
+   
+    }
+   
+    // Update is called once per frame
+    void Update () {
+ 
+        if(isDead) {
+            return;
+        }
+ 
+        score += Time.deltaTime;
+        scoreText.text = ((int)score).ToString();
+    }
+ 
+    public void OnDeath() {
+        isDead = true;
+        deathMenu.ToggleEndScore (score);
+    }
 }
