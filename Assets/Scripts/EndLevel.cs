df7ac984192bbae4275ed34f43191f77e7b3ad0e
Mikael Ahlström <mikael.ahlstrom@metropolia.fi>
2016-12-05 10:40:34 +0200

    EndLevel.cs

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/*
+This script ends levels and moves to the next one.
+Authors:
+Mikael Ahlström
+Kendy Nguyen
+*/
+public class EndLevel : MonoBehaviour {
+
+	private float timeLeft = 360;
+	
+	// Update is called once per frame
+	void Update () {
+	
+		timeLeft -= Time.deltaTime;
+    	if ( timeLeft < 0 )
+    	{
+         SceneManager.LoadScene("Name Of Scene");
+    	}
+	}
+}
