bdbde411807d32086915426b8bc5bae3329802cc
Kendy Nguyen <kendynguyen20@hotmail.com>
2016-12-05 13:04:22 +0200

    PauseMenu

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -4,91 +4,88 @@ using System.Collections;
 using UnityEngine.UI;
 
 /* Author: Kendy Nguyen
- * Tämä script keskeyttää pelin painamalla
- * Pause logoa vasemmasta ylänurkasta.
- * Koodi asettaa pelin ajaksi 0
- * ja tulostaa canvaksen näytölle missä
- * on pause menu.
+ * This script will pause the game
+ * by pressing "PauseLogo". PauseGame
+ * script will freeze player and enemies
+ * and animations and print out pause canvas.
+ * Pause canvas has "Resume", "Restart"
+ * and "Main Menu" buttons.
  */ 
 
 public class PauseGame : MonoBehaviour {
 
-//----------Variables--------------------------------------------------------------------------------------------------------------------------------------------
+	//----------Variables--------------------------------------------------------------------------------------------------------------------------------------------
 	public Button next;
 	public Button pause;													// pause
 	public Button resume;													// resume
-	private GameObject respect;
-	private GameObject score;
+	private GameObject respect;												// respect
+	private GameObject score;												// score
 	private GameObject tausta;												// tausta
 	private GameObject taustaUp;											// taustaUp
 	private GameObject taustaDown;											// taustaDown
 	private GameObject erikMate;											// erikMate
-	private GameObject enemies;		
-	private GameObject dialogi;	// enemies
+	private GameObject enemies;												// enemies
 	public bool esimerkki = false;											// bool esimerkki
 
-//-----------Start-----------------------------------------------------------------------------------------------------------------------------------------------
+	//-----------Start-----------------------------------------------------------------------------------------------------------------------------------------------
 
 	void Start (){
-		dialogi = GameObject.Find ("DialogBox");	
-		tausta = GameObject.Find ("Pause Background");						// 
-		taustaUp = GameObject.Find ("UpButton");							//
-		taustaDown = GameObject.Find ("DownButton");						//
-		respect = GameObject.Find ("Respect");
-		score = GameObject.Find ("Score");
-		erikMate = GameObject.Find ("ErikPlayer");							//
-		enemies = GameObject.Find ("AllEnemies");							//
-		pause = GameObject.Find ("PauseLogo").GetComponent<Button> ();		// 
-		resume = GameObject.Find ("Resume").GetComponent<Button> ();
-		next = GameObject.Find ("NextButton").GetComponent<Button> ();//										
-		Debug.Log (tausta);
 
-		pause.onClick.AddListener(()=> Pause());							// 
-		resume.onClick.AddListener(()=> Pause());							// 
-		tausta.SetActive (false);
-		dialogi.SetActive (true);//
+		tausta = GameObject.Find ("Pause Background");						// tausta		=	PauseBackground
+		taustaUp = GameObject.Find ("UpButton");							// taustaUp		=	UpButton
+		taustaDown = GameObject.Find ("DownButton");						// taustaDown	=	DownButtoon
+		respect = GameObject.Find ("Respect");								// respect		=	Respect
+		score = GameObject.Find ("Score");									// score		=	Score
+		erikMate = GameObject.Find ("ErikPlayer");							// erikMate		=	ErikPlayer
+		enemies = GameObject.Find ("AllEnemies");							// enemies		=	AllEnemies
+		pause = GameObject.Find ("PauseLogo").GetComponent<Button> ();		// pause		=	PauseLogo
+		resume = GameObject.Find ("Resume").GetComponent<Button> ();		// resume		=	Resume			
+		Debug.Log (tausta);													// Debug.Log	=	PauseBackground
+
+		pause.onClick.AddListener(()=> Pause());							// PauseLogo => Pause
+		resume.onClick.AddListener(()=> Pause());							// Resume => !Pause
+		tausta.SetActive (false);											// PauseBackground.SetActive (off)
 
 	}
 
-//----------Update------------------------------------------------------------------------------------------------------------------------------------------------
+	//----------Update------------------------------------------------------------------------------------------------------------------------------------------------
 
 	void Update(){
-		if (esimerkki)														// jos (esimerkki)
-		{
-								
-			tausta.SetActive(true);											// tausta.SetACtive (Canvas) on tosi eli pause menu on päällä
-			taustaUp.SetActive(false);										// 				
-			taustaDown.SetActive(false);									//
-			respect.SetActive (false);
-			score.SetActive (false);
-			Time.timeScale = 0;												// asettaa pelin ympäristölle ajaksi 0
+		if (esimerkki)														// boolean
+		{					
+			tausta.SetActive(true);											// PauseBackground	(on)
+			taustaUp.SetActive(false);										// UpButton			(off)
+			taustaDown.SetActive(false);									// DownButton		(off)
+			respect.SetActive (false);										// Respect			(off)
+			score.SetActive (false);										// Score			(off)
+			Time.timeScale = 0;												// Time set to 0
 		}
 
-		else  																// muuten
+		else  																//
 		{									
-			tausta.SetActive(false);										// tausta.SetACtive (Canvas) on epätosi eli pause menu menee pois päältä
-			taustaUp.SetActive(true);										// 
-			taustaDown.SetActive(true);										//
-			respect.SetActive (true);
-			score.SetActive (true);
-			Time.timeScale = 1;												// asettaa pelin ympäristölle ajaksi 1 eli normaali aika
+			tausta.SetActive(false);										// PauseBackground	(off)
+			taustaUp.SetActive(true);										// UpButton			(on)
+			taustaDown.SetActive(true);										// DownButton		(on)
+			respect.SetActive (true);										// Respect			(on)
+			score.SetActive (true);											// Score			(on)
+			Time.timeScale = 1;												// Time set to 1
 		}	
 	}
 
 
-//-----------Pause------------------------------------------------------------------------------------------------------------------------------------------------
+	//-----------Pause------------------------------------------------------------------------------------------------------------------------------------------------
 
 	public void Pause(){
-		esimerkki = !esimerkki;												//
+		esimerkki = !esimerkki;												// boolean = !boolean
 	}
 
 	public void Restart (){
-		Time.timeScale = 1;													// asettaa ajaksi 1 sen jälkeen kun pause menu on sulkeutunut
-		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);		// lataa scenen uudestaan
+		Time.timeScale = 1;													// Time set to 1
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);		// LoadScene = Level.name
 	}
 
 	public void MainMenu (){												
-		SceneManager.LoadScene ("StartMenu");								// lataa "StartMenu" scenen
+		SceneManager.LoadScene ("StartMenu");								// LoadScene = StartMenu
 	}
 }
 
