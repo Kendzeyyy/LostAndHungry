b887ac7027d64fc903c6cc50f5f48614e059ab12
Mikael Ahlström <mikael.ahlstrom@metropolia.fi>
2016-12-07 10:47:50 +0200

    FlappyGenerator tweaks

--- Assets/Scripts/FlappyGenerator.cs
+++ Assets/Scripts/FlappyGenerator.cs
@@ -12,7 +12,7 @@ public class FlappyGenerator : MonoBehaviour {
 	public GameObject obstacle;
 
 	// Obstacle generation start point.
-	float x = 6;
+	float x = 14;
 
 	void Update () {
 
@@ -21,7 +21,7 @@ public class FlappyGenerator : MonoBehaviour {
 
 		// Sets the max amount of generated obstacles and space between them.
 		if(x < 100) {
-			Instantiate(obstacle, new Vector3(x * 20.0f, y, 0),Quaternion.identity);
+			Instantiate(obstacle, new Vector3(x * 9.0f, y, 0),Quaternion.identity);
 			x++;
 		}
 		Debug.Log(x);
