55806e4078cd6907c8605bb1fb689480a8d5ff71
JennaMetro <jenna.kopra@metropolia.fi>
2016-12-08 16:28:49 +0200

    camera2 kommentti

--- Assets/Scripts/CameraFollow2.cs
+++ Assets/Scripts/CameraFollow2.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Kameran seuraus
+// Author: Jenna Kopra
+
 public class CameraFollow2 : MonoBehaviour {
 
 	public GameObject targetErik;									//Erikin hahmo
@@ -10,7 +13,7 @@ public class CameraFollow2 : MonoBehaviour {
 	void Update () {
 		float targetObjectX = targetErik.transform.position.x;		//Kertoo mikä on targetObjectX
 		Vector3 newCameraPosition = transform.position;				// Kertoo mikä on newCameraPosition
-		newCameraPosition.x = targetObjectX+=600;					// Seuraa kohdetta Erik + 11 sivuttain, jotta Erik ei olisi kuvan keskellä
+		newCameraPosition.x = targetObjectX+=600;					// Seuraa kohdetta Erik + 600 sivuttain, jotta Erik ei olisi kuvan keskellä
 		transform.position = newCameraPosition;						// Liikuttaa kameraa
 
 	}
